005e5cc7f93bb6e4e359f1c98ab14118639be7a8
KumHoe Lau <nerojz86@gmail.com>
2022-02-20 15:01:07 +0800

    Update UI test option

--- selenium/ui-test.py
+++ selenium/ui-test.py
@@ -15,9 +15,9 @@ def logging(msg):
 def login (user, password):
     print ('Starting the browser...')
     # --uncomment when running in Azure DevOps.
-    # options = ChromeOptions()
-    # options.add_argument("--headless") 
-    # driver = webdriver.Chrome(options=options)
+    options = ChromeOptions()
+    options.add_argument("--headless") 
+    driver = webdriver.Chrome(options=options)
     driver = webdriver.Chrome()
     logging("Browser started successfully. Navigating to the demo page to login.")
     driver.get('https://www.saucedemo.com/')
